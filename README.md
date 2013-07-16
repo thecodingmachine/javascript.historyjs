0bc226e33076c60949e80c145ac30b4825eadcfc
David Négrier <d.negrier@thecodingmachine.com>
2013-07-16 23:28:02 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+jqBootstrapValidation library
+=============================
+
+This is a simple Composer package that will install History.js in your Composer project.
+It will also create a "Mouf" instance for that plugin (if you are using Mouf).
+
+More about this library on its homepage: https://github.com/browserstate/history.js
---
+++ install.php
@@ -0,0 +1,27 @@
+<?php
+use Mouf\Html\Utils\WebLibraryManager\WebLibraryInstaller;
+
+require_once __DIR__."/../../autoload.php";
+
+use Mouf\Actions\InstallUtils;
+use Mouf\MoufManager;
+
+// Let's init Mouf
+InstallUtils::init(InstallUtils::$INIT_APP);
+
+// Let's create the instance
+$moufManager = MoufManager::getMoufManager();
+
+WebLibraryInstaller::installLibrary("jQueryLibrary",
+	array('vendor/mouf/javascript.historyjs/scripts/bundled/html4+html5/jquery.history.js'),
+	array(),
+	array(),
+	true
+);
+
+
+// Let's rewrite the MoufComponents.php file to save the component
+$moufManager->rewriteMouf();
+
+// Finally, let's continue the install
+InstallUtils::continueInstall();
